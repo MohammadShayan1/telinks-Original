a7d6abc54f60b1d0d739c4821122a075247a2fbd
Mohammad Shayan <officialm.shayan@gmail.com>
2024-09-29 00:26:26 +0500

    gallery js changes

--- gui/js/gallery.js
+++ gui/js/gallery.js
@@ -1,17 +1,15 @@
-jQuery(document).ready(function($) {
-    // Initialize Isotope
-    var $portfolioItem = $('.portfolio-item').isotope({
+$(document).ready(function () {
+    var $grid = $('.portfolio-item').isotope({
         itemSelector: '.item',
         layoutMode: 'fitRows'
     });
 
     // Filter items on button click
-    $('.portfolio-menu ul li').on('click', function() {
+    $('.portfolio-menu ul').on('click', 'li', function () {
+        var filterValue = $(this).attr('data-filter');
+        $grid.isotope({ filter: filterValue });
         $('.portfolio-menu ul li').removeClass('active');
         $(this).addClass('active');
-
-        var selector = $(this).attr('data-filter');
-        $portfolioItem.isotope({ filter: selector });
-        return false;
     });
 });
+
